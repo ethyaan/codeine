89da94070e8f045b379756e005b85dc7e17ed984
Ethyaan <ehsan.aghaeii@gmail.com>
2023-07-14 00:19:53 +0300

    init commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['flowbite.com', "flowbite.s3.amazonaws.com"]
+    }
+}
+
+module.exports = nextConfig
---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Codeine",
+  description: "Where we discuss around the codes",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className="dark">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
