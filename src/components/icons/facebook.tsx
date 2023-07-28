1d16aa2679d1b6ac14540ac9701fa04b46e4d084
Ethyaan <ehsan.aghaeii@gmail.com>
2023-07-28 21:47:48 +0300

    chore: add Icons

---
+++ src/components/icons/facebook.tsx
@@ -0,0 +1,20 @@
+export const Facebook = () => {
+  return (
+    <svg
+      className="w-5 h-5 text-gray-500 dark:text-gray-400"
+      aria-hidden="true"
+      viewBox="0 0 18 18"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clipPath="url(#clip0_13676_82298)">
+        <path d="M18 9C18 4.02943 13.9706 0 9 0C4.02943 0 0 4.02943 0 9C0 13.4921 3.29115 17.2155 7.59375 17.8907V11.6016H5.30859V9H7.59375V7.01719C7.59375 4.76156 8.93742 3.51562 10.9932 3.51562C11.9776 3.51562 13.0078 3.69141 13.0078 3.69141V5.90625H11.873C10.755 5.90625 10.4062 6.60006 10.4062 7.3125V9H12.9023L12.5033 11.6016H10.4062V17.8907C14.7088 17.2155 18 13.4921 18 9Z"></path>
+      </g>
+      <defs>
+        <clipPath id="clip0_13676_82298">
+          <rect width="18" height="18" fill="white"></rect>
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
---
+++ src/components/icons/link.tsx
@@ -0,0 +1,18 @@
+export const Link = () => {
+  return (
+    <svg
+      className="w-5 h-5 text-gray-500 dark:text-gray-400"
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
+      ></path>
+    </svg>
+  );
+};
