af816769c7437ab9c57e333f4a0c3769acc81be4
Ethyaan <ehsan.aghaeii@gmail.com>
2023-07-29 17:34:53 +0300

    chore: use excerpt and content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Markdown from "markdown-to-jsx";
 import ButtonIcon from "@/components/ButtonIcon";
 import { useAppSelector } from "@/redux/hooks";
 import { PostType } from "@/common/types/post";
@@ -11,7 +12,7 @@ export default function Home() {
       <article className="xl:w-[52rem] lg:w-[52rem] md-w-auto w-full max-w-none format format-sm sm:format-base lg:format-lg format-blue dark:format-invert md:pr-4">
         {posts &&
           posts.length > 0 &&
-          posts.map(({ title, date }, index) => {
+          posts.map(({ title, author, authorURL, date, excerpt }, index) => {
             return (
               <div className="w-full" key={`post-${index}`}>
                 <h1 className="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">
@@ -22,10 +23,11 @@ export default function Home() {
                     <span>
                       By{" "}
                       <a
-                        href="#"
+                        href={authorURL}
+                        target="_blank"
                         className="text-gray-900 dark:text-white hover:underline no-underline font-semibold"
                       >
-                        Jese Leos
+                        {author}
                       </a>
                     </span>
                     <span className="bg-gray-300 dark:bg-gray-400 w-2 h-2 rounded-full"></span>
@@ -44,26 +46,9 @@ export default function Home() {
                     </div>
                   </aside>
                 </div>
-                <p className="lead">
-                  Flowbite is an open-source library of UI components built with
-                  the utility-first classes from Tailwind CSS. It also includes
-                  interactive elements such as dropdowns, modals, datepickers.
-                </p>
-                <p>
-                  Before going digital, you might benefit from scribbling down
-                  some ideas in a sketchbook. This way, you can think things
-                  through before committing to an actual design project.
-                </p>
-                <p>
-                  But then I found a{" "}
-                  <a href="https://flowbite.com">
-                    component library based on Tailwind CSS called Flowbite
-                  </a>
-                  . It comes with the most commonly used UI components, such as
-                  buttons, navigation bars, cards, form elements, and more which
-                  are conveniently built with the utility classes from Tailwind
-                  CSS.
-                </p>
+                <div>
+                  <Markdown>{excerpt}</Markdown>
+                </div>
               </div>
             );
           })}
--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import Markdown from "markdown-to-jsx";
 import { getPost } from "@/utils/post";
 import ClientPost from "./clientPost";
 import { PostType } from "@/common/types/post";
@@ -6,5 +7,13 @@ export default function Post(props: any) {
   const slug = props.params.slug;
   const post: PostType = getPost(slug);
 
-  return <ClientPost post={post}>Hello</ClientPost>;
+  return (
+    <ClientPost post={post}>
+      <article className="xl:w-[52rem] lg:w-[52rem] md-w-auto w-full max-w-none format format-sm sm:format-base lg:format-lg format-blue dark:format-invert md:pr-4">
+        <div className="w-full">
+          <Markdown>{post.content}</Markdown>
+        </div>
+      </article>
+    </ClientPost>
+  );
 }
