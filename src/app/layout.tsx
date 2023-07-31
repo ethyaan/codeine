2dd220a48667691f8df9f6cda5de60e6c3b90981
Ethyaan <ehsan.aghaeii@gmail.com>
2023-07-31 19:27:57 +0300

    chore: remove flowbite js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,6 @@ export default function RootLayout({
 }) {
   return (
     <>
-      {/* <Script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.js" /> */}
       <Script id="theme-color">{`
         if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
           document.documentElement.classList.add('dark');
--- src/components/ButtonIcon.tsx
+++ src/components/ButtonIcon.tsx
@@ -14,21 +14,18 @@ export default function ButtonIcon({ icon, tooltip, onClick }: any) {
   return (
     <>
       <button
-        data-tooltip-target={`tooltip-${icon}`}
-        className="inline-flex items-center p-2 mx-0.5 text-sm font-medium text-center text-gray-500 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-2 dark:text-white focus:ring-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        className="group relative inline-flex items-center p-2 mx-0.5 text-sm font-medium text-center text-gray-500 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-2 dark:text-white focus:ring-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
         type="button"
         onClick={onClick}
       >
         <IconComponent />
+        <div className="group-hover:opacity-100 transition-opacity font-medium text-white absolute top-0 left-1/2 -translate-x-1/2 -translate-y-full opacity-0 flex flex-col items-center">
+          <span className="py-2 px-3 text-sm bg-gray-900 dark:bg-gray-700 rounded-lg shadow-sm mx-auto whitespace-pre">
+            {tooltip}
+          </span>
+          <span className="h-0 w-0 border-x-4 border-x-transparent border-t-[8px] border-t-gray-900 dark:border-t-gray-700"></span>
+        </div>
       </button>
-      <div
-        id={`tooltip-${icon}`}
-        role="tooltip"
-        className="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700"
-      >
-        {tooltip}
-        <div className="tooltip-arrow" data-popper-arrow=""></div>
-      </div>
     </>
   );
 }
