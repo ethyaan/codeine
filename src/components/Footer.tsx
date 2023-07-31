8c3bd688c9853d1655dc5fe2e815298cd87754b4
Ethyaan <ehsan.aghaeii@gmail.com>
2023-07-31 18:16:21 +0300

    chore: update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
         <div className="sm:flex sm:items-center sm:justify-between">
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
             {`${new Date().getFullYear()} `}
-            <a href="#" className="hover:underline">
+            <a href="https://codeine.blog" className="hover:underline">
               Codeine
             </a>
             . All Rights Reversed.
